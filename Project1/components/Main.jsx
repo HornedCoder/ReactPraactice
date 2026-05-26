0cec77b86e8cf6a13c3cf7c118d004531446e47a
HornedCoder <kayush5u2018@gmail.com>
2026-05-26 13:24:13 +0530

    Add files via upload

---
+++ Project1/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return(
+        <main>
+            <h1>Fun Facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well overr 100K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprises apps, including Mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ Project1/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import ReactLogo from 'C:/ReactProjects/project1/src/assets/react.svg'
+export default function Navbar() {
+    return(
+        <header>
+            <nav>
+                <img src= {ReactLogo}></img>
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
